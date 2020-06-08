aaa962b5145dfbdd999b4b98f83ed6958f3b7330
hellbringersbrother <timothyforrestermurray@gmail.com>
2020-06-08 18:01:05 +1200

    Made it extract numbers and sort the numbers. Then it uses the numbers to compare to the original list and grabs filenames to use for extracting contents.

--- Files.py
+++ Files.py
@@ -22,75 +22,82 @@ def find_files(dir, dict, filenames, folders, old_folder, i):
 	i+=1
 	return i
 
-
+# extracts the number from the filename, uses it for sorting
+def extract_number(filename):
+	num_str = ""
+	for char in filename:
+		if(char in "0123456789"):
+			num_str += char
+	num = int(num_str)
+	return num
 # This function will return true if file is valid, false if invalid
-def validate_filename(filename):
-	name = filename[:-4]
-	extension = filename[-4:]
-	namesplit = name.split("-")
+# def validate_filename(filename):
+	# name = filename[:-4]
+	# extension = filename[-4:]
+	# namesplit = name.split("-")
 
-	# Checking the name of the file is split into 3 sections
-	if len(namesplit) != 3:
-		return False
-	# Checking all sections of the filename are numerical
-	for s in namesplit:
-		if not s.isnumeric():
-			return False
+	# # Checking the name of the file is split into 3 sections
+	# if len(namesplit) != 3:
+		# return False
+	# # Checking all sections of the filename are numerical
+	# for s in namesplit:
+		# if not s.isnumeric():
+			# return False
 
-	job_site = int(namesplit[0])
-	lab_desk = int(namesplit[1])
-	job_num = int(namesplit[2])
+	# job_site = int(namesplit[0])
+	# lab_desk = int(namesplit[1])
+	# job_num = int(namesplit[2])
 	
-	# create an id using job_site and lab_desk, with a number to count how many there are
-	file_id = [namesplit[0]+namesplit[1], 1]
-	# how we check the thing doesn't exist
-	count = 0
-	for id in unique_ids:
-		# if the id is already in unique_ids
-		if(file_id[0] == id[0]):
-			# increment the number of times the id shows up
-			id[1] += 1
+	# # create an id using job_site and lab_desk, with a number to count how many there are
+	# file_id = [namesplit[0]+namesplit[1], 1]
+	# # how we check the thing doesn't exist
+	# count = 0
+	# for id in unique_ids:
+		# # if the id is already in unique_ids
+		# if(file_id[0] == id[0]):
+			# # increment the number of times the id shows up
+			# id[1] += 1
 			
-			if(job_num != id[1]):
-				print(id[1], job_num, "broken", id[0])
-				return False
-			else:
-				print(id[1], job_num)
-		else:
-			# otherwise increment the count
-			count+= 1
-	# if we haven't seen the id
-	if(count == len(unique_ids)):
-		# add it
-		unique_ids.append(file_id)
-		if(job_num != 1):
-			print(id[1], job_num)
-			return False
+			# if(job_num != id[1]):
+				# print(id[1], job_num, "broken", id[0])
+				# return False
+			# else:
+				# print(id[1], job_num)
+		# else:
+			# # otherwise increment the count
+			# count+= 1
+	# # if we haven't seen the id
+	# if(count == len(unique_ids)):
+		# # add it
+		# unique_ids.append(file_id)
+		# if(job_num != 1):
+			# print(id[1], job_num)
+			# return False
 	
 	
 	# This code checks that the job numbers are in the valid range
 	# Does not check that job_num increases in sequential order
 	#todo: Account for leading 0 in numbers < 10 e.g. job_num should be 04, not 4
 	#todo: probably need to do some bs string manipulation
-	if job_site < 1 or job_site > 5:
-		return False
-	elif lab_desk < 1 or lab_desk > 25:
-		return False
-	elif job_num < 1 or job_num > 99:
-		return False
+	# if job_site < 1 or job_site > 5:
+		# return False
+	# elif lab_desk < 1 or lab_desk > 25:
+		# return False
+	# elif job_num < 1 or job_num > 99:
+		# return False
 
-	# This code accounts for when the number is between 1 and 9 inclusive, it checks that
-	# the actual string has a leading 0
-	if namesplit[0][0] != '0':
-		return False
-	elif lab_desk < 10 and namesplit[1][0] != '0':
-		return False
-	elif job_num < 10 and namesplit[2][0] != '0':
-		return False
+	# # This code accounts for when the number is between 1 and 9 inclusive, it checks that
+	# # the actual string has a leading 0
+	# if namesplit[0][0] != '0':
+		# return False
+	# elif lab_desk < 10 and namesplit[1][0] != '0':
+		# return False
+	# elif job_num < 10 and namesplit[2][0] != '0':
+		# return False
 
-	if extension != ".txt":
-		return False
-	return True
+	# if extension != ".txt":
+		# return False
+	# return True
 
 
 # gonna try to get the directory inputted into something
@@ -99,38 +106,46 @@ folders = []
 file_names = []
 all_dict = {}
 folders = []
+numbers = []
 
 directory = os.scandir(input_dir)
 find_files(directory, all_dict, file_names, folders, input_dir, 0)
 #print(folders)
 # Sort our array of filenames that we hopefully read in
-file_names.sort()
 
+i = 0
+while i < len(file_names):
+	numbers.append(extract_number(file_names[i]))
+	file_names[i] = [file_names[i], numbers[i]]
+	i+=1
+numbers.sort()
+file_names.sort()
+print(file_names)
 # From here we've read in all our data, and are ready to manipulate it:
 
-# These arrays contain valid files and their corresponding input
-valid_files = []
-valid_content = []
+# # These arrays contain valid files and their corresponding input
+# valid_files = []
+# valid_content = []
 
-# The same but for invalid files and their corresponding input
-invalid_files = []
-invalid_content = []
-validity = []
-# list of unique jobsites+labdesks
-unique_ids = []
-for i in range(len(file_names)):
-	# If our current file has a valid name, add it to our valid files array
-	# and add the corresponding content to the valid files array
-	if validate_filename(file_names[i]):
-		valid_files.append(file_names[i])
-		valid_content.append(all_dict[file_names[i]])
-	else:
-		# If we end up here, our filename is not valid, so add the filename and its contents to
-		# corresponding invalid arrays
-		invalid_files.append(file_names[i])
-		invalid_content.append(all_dict[file_names[i]])
+# # The same but for invalid files and their corresponding input
+# invalid_files = []
+# invalid_content = []
+# validity = []
+# # list of unique jobsites+labdesks
+# unique_ids = []
+# for i in range(len(file_names)):
+	# # If our current file has a valid name, add it to our valid files array
+	# # and add the corresponding content to the valid files array
+	# if validate_filename(file_names[i]):
+		# valid_files.append(file_names[i])
+		# valid_content.append(all_dict[file_names[i]])
+	# else:
+		# # If we end up here, our filename is not valid, so add the filename and its contents to
+		# # corresponding invalid arrays
+		# invalid_files.append(file_names[i])
+		# invalid_content.append(all_dict[file_names[i]])
 
-print(unique_ids)
+# print(unique_ids)
 
 #for s in sorted(all_dict.keys()):
 	#print(s, "validity:", validate_filename(s))
@@ -142,27 +157,34 @@ print(unique_ids)
 write_f = open('result.txt', 'w')
 write_f.close()
 write_f = open('result.txt', 'a')
-for file in valid_files:
-	file_string = input_dir + '/' + file
-	try:
-		read_f = open(file_string, 'r')
-	except:
-		# if it fails, means the file isn't in the folder, 
-		# so look through the other folders
-		i = 0
-		# folders[len-1] = base level
-		while i < len(folders)-1:
-			j = 0
-			# last item is the folder
-			while j < len(folders[i])-1:
-				if(file == folders[i][j]):
-					file_string = folders[i][len(folders[i])-1] + '/' + file
-					read_f = open(file_string, 'r')
-				j+=1
-			i+=1
-	
-	
-	write_f.write(read_f.read())
-	write_f.write('\n')
-	read_f.close()
+
+i = 0
+while i < len(numbers):
+	for file in file_names:
+		print(file[1], numbers[i])
+		if(file[1] == numbers[i]):
+			file_string = input_dir + '/' + file[0]
+			print(file_string)
+			try:
+				read_f = open(file_string, 'r')
+				print("read_opened\n")
+			except:
+				print("read_failed")
+				j = 0
+				while j < len(folders)-1:
+					k = 0
+					while k < len(folders[j])-1:
+						if(file[0] == folders[j][k]):
+							file_string = folders[j][len(folders[j])-1] + '/' + file[0]
+							read_f = open(file_string, 'r')
+							print("read_opened\n")
+							# adding breaks to make process more efficient
+							break
+						k+=1
+					j+=1
+			write_f.write(read_f.read())
+			write_f.write('\n')
+			read_f.close()
+			break
+	i+=1
 write_f.close()
