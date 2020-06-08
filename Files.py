360180924bc6957f4484088ed2c4f89776844257
hellbringersbrother <timothyforrestermurray@gmail.com>
2020-06-09 11:39:07 +1200

    Removed comments

--- Files.py
+++ Files.py
@@ -30,74 +30,6 @@ def extract_number(filename):
 			num_str += char
 	num = int(num_str)
 	return num
-# This function will return true if file is valid, false if invalid
-# def validate_filename(filename):
-	# name = filename[:-4]
-	# extension = filename[-4:]
-	# namesplit = name.split("-")
-
-	# # Checking the name of the file is split into 3 sections
-	# if len(namesplit) != 3:
-		# return False
-	# # Checking all sections of the filename are numerical
-	# for s in namesplit:
-		# if not s.isnumeric():
-			# return False
-
-	# job_site = int(namesplit[0])
-	# lab_desk = int(namesplit[1])
-	# job_num = int(namesplit[2])
-	
-	# # create an id using job_site and lab_desk, with a number to count how many there are
-	# file_id = [namesplit[0]+namesplit[1], 1]
-	# # how we check the thing doesn't exist
-	# count = 0
-	# for id in unique_ids:
-		# # if the id is already in unique_ids
-		# if(file_id[0] == id[0]):
-			# # increment the number of times the id shows up
-			# id[1] += 1
-			
-			# if(job_num != id[1]):
-				# print(id[1], job_num, "broken", id[0])
-				# return False
-			# else:
-				# print(id[1], job_num)
-		# else:
-			# # otherwise increment the count
-			# count+= 1
-	# # if we haven't seen the id
-	# if(count == len(unique_ids)):
-		# # add it
-		# unique_ids.append(file_id)
-		# if(job_num != 1):
-			# print(id[1], job_num)
-			# return False
-	
-	
-	# This code checks that the job numbers are in the valid range
-	# Does not check that job_num increases in sequential order
-	#todo: Account for leading 0 in numbers < 10 e.g. job_num should be 04, not 4
-	#todo: probably need to do some bs string manipulation
-	# if job_site < 1 or job_site > 5:
-		# return False
-	# elif lab_desk < 1 or lab_desk > 25:
-		# return False
-	# elif job_num < 1 or job_num > 99:
-		# return False
-
-	# # This code accounts for when the number is between 1 and 9 inclusive, it checks that
-	# # the actual string has a leading 0
-	# if namesplit[0][0] != '0':
-		# return False
-	# elif lab_desk < 10 and namesplit[1][0] != '0':
-		# return False
-	# elif job_num < 10 and namesplit[2][0] != '0':
-		# return False
-
-	# if extension != ".txt":
-		# return False
-	# return True
 
 
 # gonna try to get the directory inputted into something
@@ -120,39 +52,7 @@ while i < len(file_names):
 	i+=1
 numbers.sort()
 file_names.sort()
-# From here we've read in all our data, and are ready to manipulate it:
 
-# # These arrays contain valid files and their corresponding input
-# valid_files = []
-# valid_content = []
-
-# # The same but for invalid files and their corresponding input
-# invalid_files = []
-# invalid_content = []
-# validity = []
-# # list of unique jobsites+labdesks
-# unique_ids = []
-# for i in range(len(file_names)):
-	# # If our current file has a valid name, add it to our valid files array
-	# # and add the corresponding content to the valid files array
-	# if validate_filename(file_names[i]):
-		# valid_files.append(file_names[i])
-		# valid_content.append(all_dict[file_names[i]])
-	# else:
-		# # If we end up here, our filename is not valid, so add the filename and its contents to
-		# # corresponding invalid arrays
-		# invalid_files.append(file_names[i])
-		# invalid_content.append(all_dict[file_names[i]])
-
-# print(unique_ids)
-
-#for s in sorted(all_dict.keys()):
-	#print(s, "validity:", validate_filename(s))
-	#pass
-# basic idea behind reading files
-#read_f = open('BaseCase/03-05-01.txt', 'r')
-#print(read_f.read())
-#read_f.close()
 write_f = open('result.txt', 'w')
 write_f.close()
 write_f = open('result.txt', 'a')
@@ -160,7 +60,6 @@ write_f = open('result.txt', 'a')
 i = 0
 while i < len(numbers):
 	for file in file_names:
-		print(file[1], numbers[i])
 		if(file[1] == numbers[i]):
 			file_string = input_dir + '/' + file[0]
 			try:
